2e2493478c20edbc1f8a5def75302038c949bb56
ezfx44 <nickich161@gmail.com>
2017-03-12 22:42:55 +0300

    Update matrix.cpp

--- sources/matrix.cpp
+++ sources/matrix.cpp
@@ -1,15 +1,113 @@
 #include "matrix.hpp"
-
-matrix_t::matrix_t() noexcept : rows_{0}, columns_{0}, elements_{nullptr}
+Matrix::Matrix(int side) : row(side), column(side)
 {
+	arr = new int*[row];
+	for (int i = 0; i < row; i++)
+	{
+		arr[i] = new int[column];
+		for (int j = 0; j < column; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
 }
 
-auto matrix_t::rows() -> unsigned int
+Matrix::Matrix(int r, int c) : row(r), column(c)
 {
-    return rows_;
+	arr = new int*[r];
+	for (int i = 0; i < r; i++)
+	{
+		arr[i] = new int[c]{0};
+	}
 }
 
-auto matrix_t::columns() -> unsigned int
+Matrix::Matrix(const Matrix& a) :row(a.row), column(a.column)
 {
-    return columns_;
+	arr = new int*[row];
+	for (int i = 0; i < row; i++)
+	{
+		arr[i] = new int[column];
+		for (int j = 0; j < column; j++)
+		{
+			arr[i][j] = a.arr[i][j];
+		}
+	}
+}
+
+Matrix::~Matrix()
+{
+	for (int i = 0; i < row; i++) {
+		delete[] arr[i];
+	}
+	delete[] arr;
+}
+
+void Matrix::fill_matrix(const char* name)
+{
+	std::ifstream fin(name);
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < column; j++)
+		{
+			fin >> arr[i][j];
+		}
+	}
+}
+
+void Matrix::show_matrix() const
+{
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < column; j++)
+		{
+			std::cout.width(4);
+			std::cout << arr[i][j] << " ";
+		}
+		std::cout << '\n';
+	}
+	std::cout << std::endl;
+}
+
+Matrix Matrix::operator+(const Matrix& m) const
+{
+	Matrix help(row, column);
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < column; j++)
+		{
+			help.arr[i][j] = arr[i][j] + m.arr[i][j];
+		}
+	}
+	return help;
+}
+
+
+
+Matrix Matrix::operator*(const Matrix& m) const
+{
+	Matrix help(row, column);
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < column; j++)
+		{
+			help.arr[i][j] = 0;
+			for (int k = 0; k < m.row; k++)
+			{
+				help.arr[i][j] += arr[i][k] * m.arr[k][j];
+			}
+		}
+	}
+	return help;
+}
+
+Matrix Matrix::operator=(const Matrix& x)
+{
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < column; j++)
+		{
+			arr[i][j] = x.arr[i][j];
+		}
+	}
+	return *this;
 }
